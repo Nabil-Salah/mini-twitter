b5f5132a40ecc640814aabfbf02669f8510bcbba
Nabil-Salah <nabil.salah203@gmail.com>
2023-08-27 21:13:43 +0300

    Initial commit

---
+++ src/main/java/com/minitwitter/minitwitter/MiniTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.minitwitter.minitwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minitwitter/minitwitter/MiniTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minitwitter.minitwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
